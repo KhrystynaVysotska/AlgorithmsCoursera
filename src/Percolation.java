b0933f0abc1f38077faef54e6104abc30d34ccca
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-07-07 22:29:23 +0300

    Updated api in order to use grid starting with 1, fixed some minor bugs

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,9 +1,9 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int[][] matrix;
+    private final int[][] matrix;
     private boolean[][] isOpen;
-    private WeightedQuickUnionUF weightedquickUnionUF;
+    private final WeightedQuickUnionUF weightedquickUnionUF;
     private int numberOfOpenSites;
 
     public Percolation(int n) {
@@ -24,46 +24,49 @@ public class Percolation {
 
     public void open(int row, int col) {
         validate(row, col);
-        if (isOpen[row][col]) {
+        if (isOpen[row - 1][col - 1]) {
             return;
         } else {
-            isOpen[row][col] = true;
+            isOpen[row - 1][col - 1] = true;
             numberOfOpenSites++;
-            if (row == 0) {
-                weightedquickUnionUF.union(0, matrix[row][col]);
-            } else if (row == matrix.length - 1) {
-                weightedquickUnionUF.union(matrix[row][col], (matrix.length * matrix.length) + 1);
+            if (matrix.length == 1) {
+                weightedquickUnionUF.union(0, matrix.length * matrix.length + 1);
             }
-            if (col != 0 && isOpen(row, col - 1)) {
-                weightedquickUnionUF.union(matrix[row][col - 1], matrix[row][col]);
+            if (row - 1 == 0) {
+                weightedquickUnionUF.union(0, matrix[row - 1][col - 1]);
+            } else if (row - 1 == matrix.length - 1) {
+                weightedquickUnionUF.union(matrix[row - 1][col - 1], (matrix.length * matrix.length) + 1);
             }
-            if (col != matrix.length - 1 && isOpen(row, col + 1)) {
-                weightedquickUnionUF.union(matrix[row][col + 1], matrix[row][col]);
+            if (col - 1 != 0 && isOpen(row, col - 1)) {
+                weightedquickUnionUF.union(matrix[row - 1][col - 2], matrix[row - 1][col - 1]);
             }
-            if (row != 0 && isOpen(row - 1, col)) {
-                weightedquickUnionUF.union(matrix[row - 1][col], matrix[row][col]);
+            if (col - 1 != matrix.length - 1 && isOpen(row, col + 1)) {
+                weightedquickUnionUF.union(matrix[row - 1][col], matrix[row - 1][col - 1]);
             }
-            if (row != matrix.length - 1 && isOpen(row + 1, col)) {
-                weightedquickUnionUF.union(matrix[row + 1][col], matrix[row][col]);
+            if (row - 1 != 0 && isOpen(row - 1, col)) {
+                weightedquickUnionUF.union(matrix[row - 2][col - 1], matrix[row - 1][col - 1]);
+            }
+            if (row - 1 != matrix.length - 1 && isOpen(row + 1, col)) {
+                weightedquickUnionUF.union(matrix[row][col - 1], matrix[row - 1][col - 1]);
             }
         }
     };
 
     public boolean isOpen(int row, int col) {
         validate(row, col);
-        return isOpen[row][col];
+        return isOpen[row - 1][col - 1];
     };
 
     public boolean isFull(int row, int col) {
         validate(row, col);
-        return weightedquickUnionUF.find(0) == weightedquickUnionUF.find(matrix[row][col]);
+        return weightedquickUnionUF.find(0) == weightedquickUnionUF.find(matrix[row - 1][col - 1]);
     };
-    
+
     private void validate(int row, int col) {
-        if(row < 0 || row >= matrix.length) {
-            throw new IllegalArgumentException("row index " + row + " is not between 0 and " + (matrix.length-1));
-        } else if(col < 0 || col >= matrix.length) {
-            throw new IllegalArgumentException("coloumn index " + col + " is not between 0 and " + (matrix.length-1));
+        if (row - 1 < 0 || row - 1 >= matrix.length) {
+            throw new IllegalArgumentException("row index " + row + " is not between 1 and " + matrix.length);
+        } else if (col - 1 < 0 || col - 1 >= matrix.length) {
+            throw new IllegalArgumentException("coloumn index " + col + " is not between 1 and " + matrix.length);
         }
     }
 
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -3,9 +3,12 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private int numberOfTrials;
-    private int sizeOfGrid;
-    Percolation percolation;
+    private static final double CONFIDENCE_CONSTANT = 1.96;
+    private final int numberOfTrials;
+    private final int sizeOfGrid;
+    private Percolation percolation;
+    private double mean;
+    private double stddev;
 
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
@@ -19,43 +22,44 @@ public class PercolationStats {
         int numberOfAllSites = sizeOfGrid * sizeOfGrid;
         int row;
         int col;
-        double generalNumberOfThresholds = 0;
+        double[] generalNumberOfThresholds = new double[numberOfTrials];
         for (int i = 0; i < numberOfTrials; i++) {
             this.percolation = new Percolation(sizeOfGrid);
             while (!percolation.percolates()) {
-                row = StdRandom.uniform(sizeOfGrid);
-                col = StdRandom.uniform(sizeOfGrid);
+                row = StdRandom.uniform(1, sizeOfGrid + 1);
+                col = StdRandom.uniform(1, sizeOfGrid + 1);
                 percolation.open(row, col);
             }
-            generalNumberOfThresholds += (double) percolation.numberOfOpenSites() / numberOfAllSites;
+            generalNumberOfThresholds[i] = (double) percolation.numberOfOpenSites() / numberOfAllSites;
         }
-        return generalNumberOfThresholds / numberOfTrials;
+        this.mean = StdStats.mean(generalNumberOfThresholds);
+        return this.mean;
     }
 
     public double stddev() {
         int numberOfAllSites = sizeOfGrid * sizeOfGrid;
-        double mean = mean();
         int row;
         int col;
         double[] valuesToCalculateStddev = new double[numberOfTrials];
         for (int i = 0; i < numberOfTrials; i++) {
             this.percolation = new Percolation(sizeOfGrid);
             while (!percolation.percolates()) {
-                row = StdRandom.uniform(sizeOfGrid);
-                col = StdRandom.uniform(sizeOfGrid);
+                row = StdRandom.uniform(1, sizeOfGrid + 1);
+                col = StdRandom.uniform(1, sizeOfGrid + 1);
                 percolation.open(row, col);
             }
-            valuesToCalculateStddev[i] = (double) percolation.numberOfOpenSites() / numberOfAllSites - mean;
+            valuesToCalculateStddev[i] = (double) percolation.numberOfOpenSites() / numberOfAllSites - this.mean;
         }
-        return StdStats.stddev(valuesToCalculateStddev);
+        this.stddev = StdStats.stddev(valuesToCalculateStddev);
+        return this.stddev;
     }
 
     public double confidenceLo() {
-        return mean() - ((1.96*stddev())/Math.sqrt(numberOfTrials));
+        return this.mean - ((CONFIDENCE_CONSTANT * this.stddev) / Math.sqrt(numberOfTrials));
     }
 
     public double confidenceHi() {
-        return mean() + ((1.96*stddev())/Math.sqrt(numberOfTrials));
+        return this.mean + ((CONFIDENCE_CONSTANT * this.stddev) / Math.sqrt(numberOfTrials));
     }
 
     public static void main(String[] args) {
