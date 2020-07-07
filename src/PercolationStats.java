df49bcd6c51d2565e7e79ce1797acea18a337df7
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-07-07 20:40:18 +0300

    Deleted comments

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -7,7 +7,6 @@ public class PercolationStats {
     private int sizeOfGrid;
     Percolation percolation;
 
-    // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException("Size of grid and amount of trials must be positive values");
@@ -16,7 +15,6 @@ public class PercolationStats {
         this.numberOfTrials = trials;
     }
 
-    // sample mean of percolation threshold
     public double mean() {
         int numberOfAllSites = sizeOfGrid * sizeOfGrid;
         int row;
@@ -34,7 +32,6 @@ public class PercolationStats {
         return generalNumberOfThresholds / numberOfTrials;
     }
 
-    // sample standard deviation of percolation threshold
     public double stddev() {
         int numberOfAllSites = sizeOfGrid * sizeOfGrid;
         double mean = mean();
@@ -53,17 +50,14 @@ public class PercolationStats {
         return StdStats.stddev(valuesToCalculateStddev);
     }
 
-    // low endpoint of 95% confidence interval
     public double confidenceLo() {
         return mean() - ((1.96*stddev())/Math.sqrt(numberOfTrials));
     }
 
-    // high endpoint of 95% confidence interval
     public double confidenceHi() {
         return mean() + ((1.96*stddev())/Math.sqrt(numberOfTrials));
     }
 
-    // test client (see below)
     public static void main(String[] args) {
         int n = StdIn.readInt();
         int trials = StdIn.readInt();
