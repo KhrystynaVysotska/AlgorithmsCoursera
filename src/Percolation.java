268c3b527fa4885a4fbeda037af26fd3a743b8e0
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-07-07 20:39:58 +0300

    Deleted comments

--- src/Percolation.java
+++ src/Percolation.java
@@ -6,7 +6,6 @@ public class Percolation {
     private WeightedQuickUnionUF weightedquickUnionUF;
     private int numberOfOpenSites;
 
-    // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("size of grid must be a positive value");
@@ -23,7 +22,6 @@ public class Percolation {
         numberOfOpenSites = 0;
     };
 
-    // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
         validate(row, col);
         if (isOpen[row][col]) {
@@ -51,13 +49,11 @@ public class Percolation {
         }
     };
 
-    // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
         return isOpen[row][col];
     };
 
-    // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         validate(row, col);
         return weightedquickUnionUF.find(0) == weightedquickUnionUF.find(matrix[row][col]);
@@ -71,25 +67,11 @@ public class Percolation {
         }
     }
 
-    // returns the number of open sites
     public int numberOfOpenSites() {
         return numberOfOpenSites;
     };
 
-    // does the system percolate?
     public boolean percolates() {
         return weightedquickUnionUF.find(0) == weightedquickUnionUF.find((matrix.length * matrix.length) + 1);
-    };
-
-    // test client (optional)
-    public static void main(String[] args) {
-        Percolation percolation = new Percolation(4);
-        percolation.open(0, 2);
-        percolation.open(1, 2);
-        percolation.open(1, 3);
-        percolation.open(2, 3);
-        percolation.open(3, 3);
-        System.out.println(percolation.percolates());
-        System.out.println(percolation.numberOfOpenSites());
     };
 }
